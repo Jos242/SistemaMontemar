7269aec6bde59e87c6178934ae30ff22363e81d5
Jos242 <jose1andres1ml@gmail.com>
2023-02-21 18:45:54 -0600

    PlanCobro created

---
+++ SistemaMontemar/ApplicationCore/Services/ServicePlanCobro.cs
@@ -0,0 +1,25 @@
+﻿using Infrastructure.Models;
+using Infrastructure.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Services
+{
+    public class ServicePlanCobro : IServicePlanCobro
+    {
+        public PlanCobro GetPlanCobroById(int id)
+        {
+            IRepositoryPlanCobro repository = new RepositoryPlanCobro();
+            return repository.GetPlanCobroById(id);
+        }
+
+        public IEnumerable<PlanCobro> GetPlanCobros()
+        {
+            IRepositoryPlanCobro repository = new RepositoryPlanCobro();
+            return repository.GetPlanCobros();
+        }
+    }
+}
---
+++ SistemaMontemar/Infrastructure/Repository/RepositoryPlanCobro.cs
@@ -0,0 +1,69 @@
+﻿using Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Web.Utils;
+
+namespace Infrastructure.Repository
+{
+    public class RepositoryPlanCobro : IRepositoryPlanCobro
+    {
+        public PlanCobro GetPlanCobroById(int id)
+        {
+            PlanCobro oPlanCobro = null;
+            try
+            {
+                using (MyContext ctx = new MyContext())
+                {
+                    ctx.Configuration.LazyLoadingEnabled = false;
+
+                    oPlanCobro = ctx.PlanCobro.Where(u => u.Id == id).FirstOrDefault();
+                }
+                return oPlanCobro;
+            }
+            catch (DbUpdateException dbEx)
+            {
+                string mensaje = "";
+                Log.Error(dbEx, System.Reflection.MethodBase.GetCurrentMethod(), ref mensaje);
+                throw new Exception(mensaje);
+            }
+            catch (Exception ex)
+            {
+                string mensaje = "";
+                Log.Error(ex, System.Reflection.MethodBase.GetCurrentMethod(), ref mensaje);
+                throw;
+            }
+        }
+
+        public IEnumerable<PlanCobro> GetPlanCobros()
+        {
+            IEnumerable<PlanCobro> lista = null;
+            try
+            {
+                using (MyContext ctx = new MyContext())
+                {
+                    ctx.Configuration.LazyLoadingEnabled = false;
+
+                    lista = ctx.PlanCobro.ToList();
+                }
+                return lista;
+            }
+            catch (DbUpdateException dbEx)
+            {
+                string mensaje = "";
+                Log.Error(dbEx, System.Reflection.MethodBase.GetCurrentMethod(), ref mensaje);
+                throw new Exception(mensaje);
+            }
+            catch (Exception ex)
+            {
+                string mensaje = "";
+                Log.Error(ex, System.Reflection.MethodBase.GetCurrentMethod(), ref mensaje);
+                throw;
+            }
+        }
+
+    }
+}
